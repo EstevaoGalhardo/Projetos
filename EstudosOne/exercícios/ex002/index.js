cae9749dc8c8a4739d46e49d98a7cfe68b6ad2fa
EstevaoGalhardo <137415130+EstevaoGalhardo@users.noreply.github.com>
2023-07-04 16:23:00 -0300

    ex003

---
+++ EstudosOne/exercícios/ex002/index.js
@@ -0,0 +1,13 @@
+/*const idade = prompt("informe a sua idade:")
+
+if (idade > 18) {
+    alert("Você é maior de idade")
+} else {
+    alert("Você não é maior de idade")
+}*/
+
+const resuldado = (6 === 5) ?"Verdadeiro" : "False"
+
+console.log(resuldado)
+
+// forma abreviada 
