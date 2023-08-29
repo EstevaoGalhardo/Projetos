88669dc7d8c70f220a523ecd4102bfc11bae9510
EstevaoGalhardo <137415130+EstevaoGalhardo@users.noreply.github.com>
2023-08-29 15:23:11 -0300

    atual

---
+++ EstudosOne/Modulo React/ex004/exercicio-04/src/components/Profile/index.jsx
@@ -0,0 +1,28 @@
+import Title from "../Title";
+import styles from "./styles.module.css";
+
+export default function Profile(props) {
+  return (
+    <div className={styles.container}>
+      <img className={styles.avatar} src={props.avatar} alt={props.name} />
+      <Title>
+        <span>{props.name}</span>
+        <button>Follow</button>
+      </Title>
+      <div>{props.bio}</div>
+      <div>{props.phone}</div>
+      <div>{props.email}</div>
+      <div className={styles.links}>
+        <a href={props.githubUrl} target="_blank">
+          GitHub
+        </a>
+        <a href={props.linkedinUrl} target="_blank">
+          LinkedIn
+        </a>
+        <a href={props.twitterUrl} target="_blank">
+          Twitter
+        </a>
+      </div>
+    </div>
+  );
+}
